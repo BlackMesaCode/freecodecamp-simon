4d0bcaa263215485f0832c5c752bc18c47a5a2bb
Stephan Sum <sum@live.de>
2017-03-22 17:40:32 +0100

    init

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                {this.props.children}
+                
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import "./index.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import App from "./components/App.js";
-import Home from "./components/Home.js";
 
-
-ReactDOM.render((
-    <Router>
-        <App>
-            <Route path="/" component={Home}/>
-        </App>
-    </Router>)
-, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
